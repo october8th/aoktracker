7912adc1ffe588bde357a9add4cbeb45b9678182
NKClem <nkuhn09@gmail.com>
2018-04-09 17:53:47 -0400

    working on trying to fix heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 //set up routes folder with mongodb routes
 //const routes = require("./routes"); 
 const PORT = process.env.PORT || 3001;
